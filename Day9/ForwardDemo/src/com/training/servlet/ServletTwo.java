533a904c444cde14f5975db581a1a4a8663bb34d
Satyen Singh  🇮🇳 <satyensingh@MacBookPro.local>
2018-01-27 12:52:23 +0530

    First Commit

---
+++ Day9/ForwardDemo/src/com/training/servlet/ServletTwo.java
@@ -0,0 +1,25 @@
+package com.training.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletTwo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("This is ServletTwo");
+		String name = (String) request.getAttribute("name");
+		PrintWriter out = response.getWriter();
+		out.print("This is ServletTwo "+name);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ Day9/MVCDemo/src/com/training/view/Display.java
@@ -0,0 +1,28 @@
+package com.training.view;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.training.model.pojo.Employee;
+
+public class Display extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Employee employee = (Employee) request.getAttribute("employee");
+		PrintWriter out = response.getWriter();
+		out.print("<h1>Employee Id : "+employee.getEmpId()+"</h1>");
+		out.print("<h1>Employee Name : "+employee.getEmpName()+"</h1>");
+		out.print("<h1>Employee Salary : "+employee.getSalary()+"</h1>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
